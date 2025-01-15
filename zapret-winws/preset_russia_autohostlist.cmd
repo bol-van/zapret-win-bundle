4bf3ea9f488e92dff9aa25120e0d22ebd436bc2a
AxetNord <axetnord@gmail.com>
2025-01-15 13:50:14 +0300

    добавлена обработка исключений в autohostlist и файл исключений

--- zapret-winws/preset_russia_autohostlist.cmd
+++ zapret-winws/preset_russia_autohostlist.cmd
@@ -1,8 +1,8 @@
 start "zapret: http,https,quic" /min "%~dp0winws.exe" ^
 --wf-tcp=80,443 --wf-udp=443,50000-50099 ^
---filter-tcp=80 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%~dp0autohostlist.txt" --new ^
+--filter-tcp=80 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --hostlist-auto="%~dp0autohostlist.txt" --hostlist-exclude="%~dp0excludehostlist.txt" --new ^
 --filter-tcp=443 --hostlist="%~dp0list-youtube.txt" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls="%~dp0tls_clienthello_www_google_com.bin" --new ^
---filter-tcp=443 --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=badseq,md5sig --hostlist-auto="%~dp0autohostlist.txt" --new ^
+--filter-tcp=443 --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=badseq,md5sig --hostlist-auto="%~dp0autohostlist.txt" --hostlist-exclude="%~dp0excludehostlist.txt" --new ^
 --filter-udp=443 --hostlist="%~dp0list-youtube.txt" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic="%~dp0quic_initial_www_google_com.bin" --new ^
---filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 --hostlist="%~dp0autohostlist.txt" --new ^
+--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 --hostlist="%~dp0autohostlist.txt" --hostlist-exclude="%~dp0excludehostlist.txt" --new ^
 --filter-udp=50000-50099 --ipset="%~dp0ipset-discord.txt" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-any-protocol --dpi-desync-cutoff=n4
